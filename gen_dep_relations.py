6c4fae05cbc545d2f7e17c12618d309b9961e631
Chirag Soni <chirag.soni9352@gmail.com>
2017-10-13 03:06:50 +0530

    combine dependency file genration in one file

--- gen_dep_relations.py
+++ gen_dep_relations.py
@@ -1,7 +1,46 @@
 from nltk.parse.stanford import StanfordDependencyParser
 import pandas as pd
 import argparse
-import pdb
+import pickle
+import copy
+
+def gen_dep_files(read_file,op,dep_parser):
+	X = pd.read_csv(read_file,sep="\t")
+	deformed_count = 0
+	count = 0
+	rel_list = []
+	tree_list = []
+		
+	with open(args.data+op+"_deformed_sentences.txt","w") as d:
+		for x in X['Message']:
+			count += 1
+			print(op+" instance = " + str(count))
+			try:
+				parsed = dep_parser.raw_parse(x)
+				parsed_copy = copy.deepcopy(parsed)
+				relations = [list(parse.triples()) for parse in parsed]
+				tree = [parse.tree() for parse in parsed_copy]
+				rel_list.append(relations)
+				tree_list.append(tree)
+			except Exception as e:
+				d.write(x)
+				d.write("\n")
+				deformed_count += 1
+				empty_list = []
+				rel_list.append(empty_list)
+				tree_list.append(empty_list)
+			if count == 5:
+				break
+	print(op+"_deformed_count = " + str(deformed_count))
+
+	with open(args.data+op+"_dependency_rel.p","wb") as rel:
+		pickle.dump(rel_list,rel)
+
+	with open(args.data+op+"_dependency_tree.p","wb") as tr:
+		pickle.dump(tree_list,tr)
+
+	print(op+" file completed.")
+
 
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser(description='Prepare dependency parser data')
@@ -11,50 +50,9 @@ if __name__ == "__main__":
 
 	trainF = args.data + "train.tsv"
 	testF = args.data + "test.tsv"
-	tuneF = args.data + "tune.tsv"    
+	# tuneF = args.data + "tune.tsv"    
 
 	dep_parser = StanfordDependencyParser(model_path="edu/stanford/nlp/models/lexparser/englishPCFG.ser.gz")
 
-	X = pd.read_csv(testF,sep="\t")
-	test_deformed_count = 0
-	count = 0
-	with open(args.data+"test_dependency.txt","w") as f:
-		with open(args.data+"test_deformed_sentences.txt","w") as d:
-			for x in X['Message']:
-				count += 1
-				print("testing instance = " + str(count))
-				try:
-					parsed = dep_parser.raw_parse(x)
-					dependencies = [list(parse.triples()) for parse in parsed]
-					f.write(str(dependencies))
-				except Exception as e:
-					d.write(x)
-					d.write("\n")
-					test_deformed_count += 1
-					empty_list = []
-					f.write(str(empty_list))
-				f.write("\n")
-	print("test_deformed_count = " + str(test_deformed_count))
-	print("test file completed.")
-
-	X = pd.read_csv(trainF,sep="\t")
-	train_deformed_count = 0
-	count = 0
-	with open(args.data+"train_dependency.txt","w") as f:
-		with open(args.data+"train_deformed_sentences.txt","w") as d:
-			for x in X['Message']:
-				count += 1
-				print("training instance = " + str(count))
-				try:
-					parsed = dep_parser.raw_parse(x)
-					dependencies = [list(parse.triples()) for parse in parsed]
-					f.write(str(dependencies))
-				except Exception as e:
-					d.write(x)
-					d.write("\n")
-					train_deformed_count += 1
-					empty_list = []
-					f.write(str(empty_list))
-				f.write("\n")
-	print("train_deformed_count = " + str(train_deformed_count))
-	print("train file completed.")
+	gen_dep_files(trainF,"train",dep_parser)
+	gen_dep_files(testF,"test",dep_parser)
